41290d8f57a504762c0dc07ee12b424b5df6e54d
bodette-dev <54686614+bodette-dev@users.noreply.github.com>
2019-11-21 14:01:48 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 function init(){
 //add your javascrip between these two lines of code
+ var input = document.getElementById('entryinput');
+ var output = document.getElementById('textoutput');
+ var button = document.getElementById('entrybutton');
+ 
+ button.addEventListener('click',function() {
+  alert('Blake Odette: '+input.value);
+ });
+ 
+ 
+ 
+ 
  
 
 
